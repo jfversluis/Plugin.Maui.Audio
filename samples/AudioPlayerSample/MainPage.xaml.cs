753bd55c98e6678dc92f9070ec3110be4fdb171d
Shaun Lawrence <shaunrlawrence@gmail.com>
2022-08-20 13:46:36 +0100

    Add basic position reporting/setting

--- samples/AudioPlayerSample/MainPage.xaml.cs
+++ samples/AudioPlayerSample/MainPage.xaml.cs
@@ -5,19 +5,29 @@ namespace AudioPlayerSample;
 public partial class MainPage : ContentPage
 {
     readonly IAudioManager audioManager;
-    IAudioPlayer simpleAudioPlayer;
+	readonly IDispatcher dispatcher;
+	IAudioPlayer simpleAudioPlayer;
+    bool isPositionChangeSystemDriven;
 
-    public MainPage(IAudioManager audioManager)
+    public MainPage(
+        IAudioManager audioManager,
+        IDispatcher dispatcher)
 	{
 		InitializeComponent();
         this.audioManager = audioManager;
-    }
+		this.dispatcher = dispatcher;
+	}
 
     async void btnPlay_Clicked(Object sender, EventArgs e)
     {
         simpleAudioPlayer = audioManager.CreatePlayer(await FileSystem.OpenAppPackageFileAsync("ukelele.mp3"));
         simpleAudioPlayer.Play();
-    }
+
+        // TODO: Possible MAUI bug as this appears to do nothing (only tested on iOS and macOS).
+        sliderPosition.Maximum = simpleAudioPlayer.Duration;
+
+		UpdatePlaybackPosition();
+	}
 
     void btnPause_Clicked(object sender, EventArgs e)
     {
@@ -28,6 +38,8 @@ public partial class MainPage : ContentPage
         else
         {
             simpleAudioPlayer.Play();
+
+            UpdatePlaybackPosition();
         }
     }
 
@@ -55,4 +67,38 @@ public partial class MainPage : ContentPage
     {
         simpleAudioPlayer.Loop = e.Value;
     }
+
+	void sliderPosition_ValueChanged(object sender, ValueChangedEventArgs e)
+	{
+        if (isPositionChangeSystemDriven)
+        {
+            return;
+        }
+
+        simpleAudioPlayer.Seek(e.NewValue);
+	}
+
+	void UpdatePlaybackPosition()
+	{
+		if (simpleAudioPlayer.IsPlaying is false)
+		{
+			return;
+		}
+
+		dispatcher.DispatchDelayed(
+			TimeSpan.FromMilliseconds(16),
+			() =>
+			{
+				Console.WriteLine($"{simpleAudioPlayer.CurrentPosition} with duration of {simpleAudioPlayer.Duration}");
+
+				isPositionChangeSystemDriven = true;
+
+				sliderPosition.Value = simpleAudioPlayer.CurrentPosition;
+				lblPosition.Text = simpleAudioPlayer.CurrentPosition.ToString();
+
+				isPositionChangeSystemDriven = false;
+
+				UpdatePlaybackPosition();
+			});
+	}
 }
