c0c83267ba010701279ef7b440d33c6c1ff7e94f
Shaun Lawrence <shaunrlawrence@gmail.com>
2022-08-20 15:14:45 +0100

    initial play with a behavior that will play an audio file

---
+++ src/Plugin.Maui.Audio/PlayAudioBehavior.cs
@@ -0,0 +1,82 @@
+﻿namespace Plugin.Maui.Audio;
+
+public class PlayAudioBehavior : Behavior<View>
+{
+	IAudioPlayer? audioPlayer;
+	readonly TapGestureRecognizer tapGestureRecognizer;
+
+	/// <summary>
+	/// Backing BindableProperty for the <see cref="AnimationType"/> property.
+	/// </summary>
+	public static readonly BindableProperty AudioFileProperty =
+		BindableProperty.Create(
+			nameof(AudioFile),
+			typeof(string),
+			typeof(PlayAudioBehavior),
+			propertyChanged: OnAudioFilePropertyChanged);
+
+	/// <summary>
+	/// The type of animation to perform.
+	/// </summary>
+	public string? AudioFile // TODO: AudioSource???
+	{
+		get => (string?)GetValue(AudioFileProperty);
+		set => SetValue(AudioFileProperty, value);
+	}
+
+	public PlayAudioBehavior()
+	{
+		tapGestureRecognizer = new TapGestureRecognizer();
+	}
+
+	private void OnTapGestureRecognizerTapped(object? sender, EventArgs e)
+	{
+		audioPlayer?.Play();
+	}
+
+	protected override void OnAttachedTo(View bindable)
+	{
+		base.OnAttachedTo(bindable);
+
+		TryCreateAudioPlayer();
+
+		tapGestureRecognizer.Tapped += OnTapGestureRecognizerTapped;
+		bindable.GestureRecognizers.Add(tapGestureRecognizer);
+	}
+
+	private static void OnAudioFilePropertyChanged(BindableObject sender, object oldValue, object newValue)
+	{
+		((PlayAudioBehavior)sender).TryCreateAudioPlayer();
+	}
+
+	protected override void OnBindingContextChanged()
+	{
+		base.OnBindingContextChanged();
+
+		TryCreateAudioPlayer();
+	}
+
+	protected override void OnDetachingFrom(View bindable)
+	{
+		base.OnDetachingFrom(bindable);
+
+		tapGestureRecognizer.Tapped -= OnTapGestureRecognizerTapped;
+		bindable.GestureRecognizers.Remove(tapGestureRecognizer);
+
+		audioPlayer?.Dispose();
+		audioPlayer = null;
+	}
+
+	private async void TryCreateAudioPlayer()
+	{
+		if (AudioFile is null || audioPlayer is not null)
+		{
+			return;
+		}
+
+		// TODO: how best to load files?
+		var fileStream = await FileSystem.OpenAppPackageFileAsync(AudioFile);
+
+		audioPlayer = AudioManager.Current.CreatePlayer(fileStream);
+	}
+}
