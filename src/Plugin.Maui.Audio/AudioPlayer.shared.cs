0491ce079b036e26c86e2575c8a842d44648c684
Matthew Clendening <matthewc@bluemarblegeo.com>
2022-09-09 14:37:28 -0400

    Add support for net6.0 (#24)

--- src/Plugin.Maui.Audio/AudioPlayer.shared.cs
+++ src/Plugin.Maui.Audio/AudioPlayer.shared.cs
@@ -2,7 +2,8 @@
 
 public partial class AudioPlayer : IAudioPlayer
 {
-    public event EventHandler? PlaybackEnded;
+#pragma warning disable CS0067
+	public event EventHandler? PlaybackEnded;
 
     ~AudioPlayer()
     {
@@ -15,4 +16,36 @@ public partial class AudioPlayer : IAudioPlayer
 
         GC.SuppressFinalize(this);
     }
+
+#if (NET6_0 && !ANDROID && !IOS && !MACCATALYST && !WINDOWS)
+
+	public AudioPlayer(Stream audioStream) {}
+
+	public AudioPlayer(string fileName) {}
+
+	protected virtual void Dispose(bool disposing) {}
+
+	public double Duration { get; }
+
+	public double CurrentPosition { get; }
+
+	public double Volume { get; set; }
+
+	public double Balance { get; set; }
+
+	public bool IsPlaying { get; }
+
+	public bool Loop { get; set; }
+
+	public bool CanSeek { get; }
+
+	public void Play() {}
+
+	public void Pause() {}
+
+	public void Stop() {}
+
+	public void Seek(double position) {}
+
+#endif
 }
