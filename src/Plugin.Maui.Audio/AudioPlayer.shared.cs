96b7d83fa771a8a818a49b34ba2116755ddff3bd
Johan Svensson <johan.svensson@cub.se>
2022-09-25 21:38:24 +0200

    Add Speed property to NET6_0 definition

--- src/Plugin.Maui.Audio/AudioPlayer.shared.cs
+++ src/Plugin.Maui.Audio/AudioPlayer.shared.cs
@@ -3,20 +3,22 @@
 public partial class AudioPlayer : IAudioPlayer
 {
 #pragma warning disable CS0067
+
 	public event EventHandler? PlaybackEnded;
+
 #pragma warning restore CS0067
 
 	~AudioPlayer()
-    {
-        Dispose(false);
-    }
+	{
+		Dispose(false);
+	}
 
-    public void Dispose()
-    {
-        Dispose(true);
+	public void Dispose()
+	{
+		Dispose(true);
 
-        GC.SuppressFinalize(this);
-    }
+		GC.SuppressFinalize(this);
+	}
 
 #if (NET6_0 && !ANDROID && !IOS && !MACCATALYST && !WINDOWS)
 
@@ -33,6 +35,8 @@ public partial class AudioPlayer : IAudioPlayer
 	public double Volume { get; set; }
 
 	public double Balance { get; set; }
+
+	public double Speed { get; set; }
 
 	public bool IsPlaying { get; }
 
