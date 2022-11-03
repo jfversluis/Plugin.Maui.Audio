d6bf5650cde752455b0f912632fae565384bc5ad
Julian Ewers <ewerspej@tcd.ie>
2022-11-03 13:57:21 +0100

    generic conditional compilation independent of .NET 6.0 target framework

--- src/Plugin.Maui.Audio/AudioPlayer.shared.cs
+++ src/Plugin.Maui.Audio/AudioPlayer.shared.cs
@@ -17,36 +17,4 @@ public partial class AudioPlayer : IAudioPlayer
 
         GC.SuppressFinalize(this);
     }
-
-#if (NET6_0 && !ANDROID && !IOS && !MACCATALYST && !WINDOWS)
-
-	public AudioPlayer(Stream audioStream) {}
-
-	public AudioPlayer(string fileName) {}
-
-	protected virtual void Dispose(bool disposing) {}
-
-	public double Duration { get; }
-
-	public double CurrentPosition { get; }
-
-	public double Volume { get; set; }
-
-	public double Balance { get; set; }
-
-	public bool IsPlaying { get; }
-
-	public bool Loop { get; set; }
-
-	public bool CanSeek { get; }
-
-	public void Play() {}
-
-	public void Pause() {}
-
-	public void Stop() {}
-
-	public void Seek(double position) {}
-
-#endif
 }
