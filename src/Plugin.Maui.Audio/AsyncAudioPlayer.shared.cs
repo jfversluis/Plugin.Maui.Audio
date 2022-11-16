9bf43f7a69053a047e4239d28254dfbc681b06b5
Shaun Lawrence <shaunrlawrence@gmail.com>
2022-11-16 21:34:16 +0000

    Attempt at getting an async audio player working

---
+++ src/Plugin.Maui.Audio/AsyncAudioPlayer.shared.cs
@@ -0,0 +1,31 @@
+﻿namespace Plugin.Maui.Audio;
+
+public class AsyncAudioPlayer
+{
+	private readonly IAudioPlayer audioPlayer;
+
+	public AsyncAudioPlayer(IAudioPlayer audioPlayer)
+	{
+		this.audioPlayer = audioPlayer;
+	}
+
+	public Task PlayAsync(CancellationToken cancellationToken)
+	{
+		var taskCompletionSource = new TaskCompletionSource<bool>();
+
+		try
+		{
+			audioPlayer.PlaybackEnded += (o, e) => taskCompletionSource.SetResult(true);
+
+			audioPlayer.Play();
+
+			return taskCompletionSource.Task;
+		}
+		catch (OperationCanceledException)
+		{
+			audioPlayer.Stop();
+		}
+
+		return Task.CompletedTask;
+	}
+}
