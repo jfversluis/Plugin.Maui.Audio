020885dc0de1ed495815a011b914f19d02383eee
Shaun Lawrence <shaunrlawrence@gmail.com>
2023-09-03 20:33:20 +0100

    Recommended way to check for permissions

--- samples/AudioPlayerSample/ViewModels/BaseViewModel.cs
+++ samples/AudioPlayerSample/ViewModels/BaseViewModel.cs
@@ -15,13 +15,27 @@ public class BaseViewModel : INotifyPropertyChanged
 
 	public static async Task<bool> CheckPermissionIsGrantedAsync<TPermission>() where TPermission : BasePermission, new()
 	{
-		if (DeviceInfo.Platform == DevicePlatform.Android)
-		{
-			var status = await Permissions.CheckStatusAsync<TPermission>();
+		PermissionStatus status = await Permissions.CheckStatusAsync<TPermission>();
 
-			return (status == PermissionStatus.Unknown) || (status == PermissionStatus.Granted);
+		if (status == PermissionStatus.Granted)
+		{
+			return true;
 		}
 
-		return true;
+		if (status == PermissionStatus.Denied && DeviceInfo.Platform == DevicePlatform.iOS)
+		{
+			// Prompt the user to turn on in settings
+			// On iOS once a permission has been denied it may not be requested again from the application
+			return false;
+		}
+
+		if (Permissions.ShouldShowRationale<TPermission>())
+		{
+			// Prompt the user with additional information as to why the permission is needed
+		}
+
+		status = await Permissions.RequestAsync<TPermission>();
+
+		return status == PermissionStatus.Granted;
 	}
 }
