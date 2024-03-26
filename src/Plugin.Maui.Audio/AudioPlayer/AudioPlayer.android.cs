5f4fae62a4ad59503bf4e32223030065c2fdd6b2
borrmann <max.borrmann@gmx.net>
2024-03-26 19:08:35 +0000

    fixes issues with seek and speed

--- src/Plugin.Maui.Audio/AudioPlayer/AudioPlayer.android.cs
+++ src/Plugin.Maui.Audio/AudioPlayer/AudioPlayer.android.cs
@@ -1,4 +1,3 @@
-using System.Runtime.Versioning;
 using Android.Content.Res;
 using Android.Media;
 using Stream = System.IO.Stream;
@@ -9,12 +8,12 @@ namespace Plugin.Maui.Audio;
 partial class AudioPlayer : IAudioPlayer
 {
 	readonly MediaPlayer player;
-	int index = 0;
 	double volume = 0.5;
 	double balance = 0;
-	string path = string.Empty;
+	string cachePath = string.Empty;
 	readonly MemoryStream? stream;
 	bool isDisposed = false;
+	AudioStopwatch stopwatch = new(TimeSpan.Zero, 1.0);
 
 	public double Duration => player.Duration <= -1 ? -1 : player.Duration / 1000.0;
 
@@ -32,39 +31,102 @@ partial class AudioPlayer : IAudioPlayer
 		set => SetVolume(Volume, balance = value);
 	}
 
-	[SupportedOSPlatform("Android23.0")]
-	public double Speed
+	bool wasPlaying = false;
+	int previousPosition = 0;
+	bool isChangingSpeed = false;
+
+	/// <summary>
+	/// Internal state machine for isPlaying. This is needed because when Reset is called, the player is not playing anymore, but in Idle state IsPlaying is not available
+	/// </summary>
+	bool isPlaying = false;
+
+
+	public void SetSpeed(double sp)
 	{
-		get => player.PlaybackParams.Speed;
-		set
+		if (!OperatingSystem.IsAndroidVersionAtLeast(23))
 		{
-			// Check if set speed is supported
-			if (CanSetSpeed)
-			{
-				// Speed on Android can be between 0 and 6
-				var speedValue = Math.Clamp((float)value, 0.0f, 6.0f);
-
-				if (float.IsNaN(speedValue))
-				{
-					speedValue = 1.0f;
-				}
-
-				player.PlaybackParams = player.PlaybackParams.SetSpeed(speedValue) ?? player.PlaybackParams;
-			}
-			else
-			{
-				throw new NotSupportedException("Set playback speed is not supported!");
-			}
+			System.Diagnostics.Trace.TraceWarning("Setting speed is only supported on Android 23 and above.");
+			return;
 		}
+
+		try
+		{
+			if (isChangingSpeed)
+			{
+				System.Diagnostics.Trace.TraceWarning("last speed update was not yet completed");
+				return;
+			}
+
+			isChangingSpeed = true;
+
+			// internal (android) speed value (float)
+			internalSpeed = Math.Clamp((float)sp, minSpeed, maxSpeed);
+
+			// shared speed value (double)
+			speed = Math.Clamp(sp, MinimumSpeed, MaximumSpeed);
+
+			// to prevent illegal exception when changing the speed on some devices,
+			// we need to change the speed while the audio is reset, see: https://stackoverflow.com/questions/39442522/setplaybackparams-causes-illegalstateexception
+
+			// store current state before resetting
+
+			previousPosition = player.CurrentPosition;
+			wasPlaying = isPlaying;
+
+			// reset
+			isPlaying = false;
+			player.Reset();
+
+			// after reset, we need to prepare the audio source again
+			PrepareAudioSource();
+
+			// now we are going to update the speed parameter
+
+			// allow defaults ensures that we can play the audio in case the pitch value is not set. When the audioplayer is paused, PlaybackParams may not be what we would like it to be
+			var parms = (player.PlaybackParams.AllowDefaults() ?? new PlaybackParams().AllowDefaults())?.SetSpeed(internalSpeed) ?? throw new ArgumentException("speed value not supported");
+
+			// this ensures that the audio will fail if the speed is not supported, otherwise android simply continues but does not play any audio
+			// https://developer.android.com/reference/android/media/PlaybackParams
+			parms.SetAudioFallbackMode((int)AudioFallbackMode.Fail);
+
+			player.PlaybackParams = parms;
+
+			// we need to update the stopwatch to reflect the new speed
+			stopwatch = new AudioStopwatch(TimeSpan.FromSeconds(previousPosition), speed);
+
+			// restore previous state of the audio player
+			player.SeekTo(previousPosition);
+
+			if (wasPlaying)
+			{
+				isPlaying = true;
+				stopwatch.Start();
+				player.Start();
+			}
+			isChangingSpeed = false;
+		}
+		finally
+		{
+			isChangingSpeed = false;
+		}		
 	}
+
+
+
+	float internalSpeed = 1.0f;
+	double speed = 1.0;
+	public double Speed => speed;
+
+	const float minSpeed = 0.0f;
+	const float maxSpeed = 2.5f;
 
 	public double MinimumSpeed => 0;
 
-	public double MaximumSpeed => 6;
+	public double MaximumSpeed => 2.5;
 
 	public bool CanSetSpeed => Android.OS.Build.VERSION.SdkInt >= Android.OS.BuildVersionCodes.M;
 
-	public bool IsPlaying => player.IsPlaying;
+	public bool IsPlaying => isPlaying;
 
 	public bool Loop
 	{
@@ -74,81 +136,76 @@ partial class AudioPlayer : IAudioPlayer
 
 	public bool CanSeek => true;
 
-	internal AudioPlayer(Stream audioStream)
+	string? file;
+
+	void PrepareAudioSource()
 	{
-		player = new MediaPlayer();
-		player.Completion += OnPlaybackEnded;
-
-		if (OperatingSystem.IsAndroidVersionAtLeast(23))
+		if(string.IsNullOrWhiteSpace(file))
 		{
-			stream = new MemoryStream();
-			audioStream.CopyTo(stream);
-			var mediaDataSource = new StreamMediaDataSource(stream);
-			player.SetDataSource(mediaDataSource);
-			player.Prepare();
-		}
-		else
-		{
-			PreparePlayerLegacy(audioStream);
-		}
-	}
-
-	internal AudioPlayer(string fileName)
-	{
-		player = new MediaPlayer();
-		player.Completion += OnPlaybackEnded;
-
-		AssetFileDescriptor afd = Android.App.Application.Context.Assets?.OpenFd(fileName)
-			?? throw new FailedToLoadAudioException("Unable to create AssetFileDescriptor.");
-
-		player.SetDataSource(afd.FileDescriptor, afd.StartOffset, afd.Length);
-
-		player.Prepare();
-	}
-
-	void PreparePlayerLegacy(Stream audioStream)
-	{
-		var documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
-
-		// It seems that on Android 5 this directory does not exist.
-		if (Directory.Exists(documentsPath) is false)
-		{
-			Directory.CreateDirectory(documentsPath);
+			throw new ArgumentException("audio source is not set");
 		}
 
-		//cache to the file system
-		path = Path.Combine(documentsPath, $"cache{index++}.wav");
-
-		DeleteFile(path);
-
-		var fileStream = File.Create(path);
-		audioStream.CopyTo(fileStream);
-		fileStream.Close();
-
-		try
-		{
-			player.SetDataSource(path);
-		}
-		catch
+		if (File.Exists(file))
 		{
 			try
 			{
+				player.SetDataSource(file);
+			}
+			catch
+			{
 				var context = Android.App.Application.Context;
-				var encodedPath = Uri.Encode(path)
+				var encodedPath = Uri.Encode(file)
 					?? throw new FailedToLoadAudioException("Unable to generate encoded path.");
 				var uri = Uri.Parse(encodedPath)
 					?? throw new FailedToLoadAudioException("Unable to parse encoded path.");
 
 				player.SetDataSource(context, uri);
 			}
-			catch
-			{
-				//return false;
-			}
+		}
+		else
+		{
+			AssetFileDescriptor afd = Android.App.Application.Context.Assets?.OpenFd(file)
+				?? throw new FailedToLoadAudioException("Unable to create AssetFileDescriptor.");
+
+			player.SetDataSource(afd.FileDescriptor, afd.StartOffset, afd.Length);
 		}
 
 		player.Prepare();
 	}
+
+	internal AudioPlayer(Stream audioStream)
+	{
+		player = new MediaPlayer();
+		player.Completion += OnPlaybackEnded;
+
+		// we always store the audio in a file in cache, as the audio stream needs to be accessed again in case the speed is changed
+		cachePath = Path.Combine(FileSystem.CacheDirectory, $"{Guid.NewGuid()}.wav");
+
+		while (File.Exists(cachePath))
+		{
+			cachePath = Path.Combine(FileSystem.CacheDirectory, $"{Guid.NewGuid()}.wav");
+		}
+
+		var fileStream = File.Create(cachePath);
+		audioStream.CopyTo(fileStream);
+		fileStream.Close();
+
+		file = cachePath;
+
+		PrepareAudioSource();
+	}
+
+
+	internal AudioPlayer(string fileName)
+	{
+		player = new MediaPlayer();
+		player.Completion += OnPlaybackEnded;
+
+		file = fileName;
+
+		PrepareAudioSource();
+	}
+
 
 	static void DeleteFile(string path)
 	{
@@ -156,7 +213,10 @@ partial class AudioPlayer : IAudioPlayer
 
 		try
 		{
-			File.Delete(path);
+			if (File.Exists(path))
+			{
+				File.Delete(path);
+			}
 		}
 		catch
 		{
@@ -167,32 +227,50 @@ partial class AudioPlayer : IAudioPlayer
 	{
 		if (IsPlaying)
 		{
-			Pause();
+			player.Pause();
 			Seek(0);
+			stopwatch.Reset();
 		}
+		else if(CurrentPosition >= Duration)
+		{
+			Seek(0);
+			stopwatch.Reset();
+		}
+
+		isPlaying = true;
 		player.Start();
+		stopwatch.Start();
 	}
 
 	public void Stop()
+	{
+		if (IsPlaying)
+		{
+			isPlaying = false;
+			player.Pause();
+		}
+
+		Seek(0);
+		stopwatch.Reset();
+		PlaybackEnded?.Invoke(this, EventArgs.Empty);
+	}
+
+	public void Pause()
 	{
 		if (!IsPlaying)
 		{
 			return;
 		}
 
-		Pause();
-		Seek(0);
-		PlaybackEnded?.Invoke(this, EventArgs.Empty);
-	}
-
-	public void Pause()
-	{
+		isPlaying = false;
 		player.Pause();
+		stopwatch.Stop();
 	}
 
 	public void Seek(double position)
 	{
 		player.SeekTo((int)(position * 1000D));
+		stopwatch = new AudioStopwatch(TimeSpan.FromSeconds(position), Speed);
 	}
 
 	void SetVolume(double volume, double balance)
@@ -212,9 +290,11 @@ partial class AudioPlayer : IAudioPlayer
 	{
 		PlaybackEnded?.Invoke(this, e);
 
+		isPlaying = player.IsPlaying;
+
 		//this improves stability on older devices but has minor performance impact
 		// We need to check whether the player is null or not as the user might have dipsosed it in an event handler to PlaybackEnded above.
-		if (Android.OS.Build.VERSION.SdkInt < Android.OS.BuildVersionCodes.M)
+		if (!OperatingSystem.IsAndroidVersionAtLeast(23))
 		{
 			player.SeekTo(0);
 			player.Stop();
@@ -234,8 +314,8 @@ partial class AudioPlayer : IAudioPlayer
 			player.Completion -= OnPlaybackEnded;
 			player.Release();
 			player.Dispose();
-			DeleteFile(path);
-			path = string.Empty;
+			DeleteFile(cachePath);
+			cachePath = string.Empty;
 			stream?.Dispose();
 		}
 
--- src/Plugin.Maui.Audio/AudioPlayer/AudioPlayer.windows.cs
+++ src/Plugin.Maui.Audio/AudioPlayer/AudioPlayer.windows.cs
@@ -1,5 +1,6 @@
 ﻿using Windows.Media.Core;
 using Windows.Media.Playback;
+using static Microsoft.Maui.ApplicationModel.Permissions;
 
 namespace Plugin.Maui.Audio;
 
@@ -24,25 +25,11 @@ partial class AudioPlayer : IAudioPlayer
         set => SetVolume(Volume, value);
     }
 
-	public double Speed
+	public double Speed => player.PlaybackSession.PlaybackRate;
+
+	public void SetSpeed(double speed)
 	{
-		get => player.PlaybackSession.PlaybackRate;
-		set
-		{
-			// Check if set speed is supported
-			if (CanSetSpeed)
-			{
-				// Windows supports between 0 and 8, but will clamp automatically for us
-				if (player.PlaybackSession.IsSupportedPlaybackRateRange(value, value))
-				{
-					player.PlaybackSession.PlaybackRate = value;
-				}
-			}
-			else
-			{
-				throw new NotSupportedException("Set playback speed is not supported!");
-			}
-		}
+		player.PlaybackSession.PlaybackRate = Math.Clamp(speed, MinimumSpeed, MaximumSpeed);
 	}
 
 	public double MinimumSpeed => 0;
@@ -72,7 +59,8 @@ partial class AudioPlayer : IAudioPlayer
 
         player.Source = MediaSource.CreateFromStream(audioStream?.AsRandomAccessStream(), string.Empty);
         player.MediaEnded += OnPlaybackEnded;
-    }
+		SetSpeed(1.0);
+	}
 
     public AudioPlayer(string fileName)
     {
@@ -85,12 +73,14 @@ partial class AudioPlayer : IAudioPlayer
 
         player.Source = MediaSource.CreateFromUri(new Uri("ms-appx:///Assets/" + fileName));
         player.MediaEnded += OnPlaybackEnded;
-    }
+		SetSpeed(1.0);
+
+	}
 
     void OnPlaybackEnded(MediaPlayer sender, object args)
     {
         PlaybackEnded?.Invoke(sender, EventArgs.Empty);
-    }
+	}
 
     public void Play()
     {
